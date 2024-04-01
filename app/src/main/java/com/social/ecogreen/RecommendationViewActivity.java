b7bb00735cf7143c38182e9ea2931e6fad7fb437
mayumi09 <it20609276@my.sliit.lk>
2024-04-01 14:34:46 +0530

    RecommendationViewActivity

---
+++ app/src/main/java/com/social/ecogreen/RecommendationViewActivity.java
@@ -0,0 +1,15 @@
+package com.social.ecogreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecommendationViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendation_view);
+    }
+
+}

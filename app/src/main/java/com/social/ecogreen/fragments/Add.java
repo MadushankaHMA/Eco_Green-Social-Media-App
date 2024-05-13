40b2ea0495071ca31ade7d00313140243fc5f612
mayumi09 <it20609276@my.sliit.lk>
2024-05-13 19:40:37 +0530

    Integration of individual component.

--- app/src/main/java/com/social/ecogreen/fragments/Add.java
+++ app/src/main/java/com/social/ecogreen/fragments/Add.java
@@ -71,7 +71,7 @@ public class Add extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        return inflater.inflate(R.layout.activity_add_post, container, false);
     }
 
     @Override
--- app/src/main/java/com/social/ecogreen/model/Users.java
+++ app/src/main/java/com/social/ecogreen/model/Users.java
@@ -54,4 +54,5 @@ public class Users {
     public void setStatus(String status) {
         this.status = status;
     }
+
 }
